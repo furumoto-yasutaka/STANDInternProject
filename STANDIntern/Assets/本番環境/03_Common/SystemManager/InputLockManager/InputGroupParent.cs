8583ca974e4428f0197c61d520f70eb488c7c4f1
furumoto-yasutaka <ika3goma3@gmail.com>
2022-10-22 11:21:34 +0900

    ・新しいステージを反映 ・背景を本番環境に統合

---
+++ STANDIntern/Assets/本番環境/03_Common/SystemManager/InputLockManager/InputGroupParent.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputGroupParent : MonoBehaviour
+{
+    protected InputGroup MyGroup;
+    protected InputLockManager inputLockManager;
+
+    public InputLockManager InputLockManager { get { return inputLockManager; } }
+
+    public bool GetIsCanInput()
+    {
+        return MyGroup.IsCanInput;
+    }
+
+    public void SetLockInputParam(InputLockManager manager, InputGroup group)
+    {
+        inputLockManager = manager;
+        MyGroup = group;
+    }
+
+    public void LockInputMyGroup()
+    {
+        inputLockManager.LockInputGroup(MyGroup.Name);
+    }
+
+    public void UnlockInputMyGroup()
+    {
+        inputLockManager.UnlockInputGroup(MyGroup.Name);
+    }
+}
